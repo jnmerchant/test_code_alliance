545775d9c7561eebd1fb805baf60bfc63fe3a9b2
jnmerchant <jnmercnc@gmail.com>
2017-03-16 11:59:22 -0400

    devise working with multiple users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :zxcvbnable
 
+  ENTITY = [['Select Entity Size', 0], ['< 50', 1], ['50 > 250', 2], ['> 250', 3]]
+
+  RESEARCHAREA = [['Select Reseach Area', 'Select Reseach Area'],
+    ['Biology', 'Biology'], ['Chemistry', 'Chemistry'],
+    ['Fishery', 'Fishery'], ['Animal Genetics', 'Animal Genetics']]
+
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
   end
+
 end
